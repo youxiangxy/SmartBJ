12f9cae31874daea41e1aff5b47d1af1e55edd2e
youxiangxy <youxiangxy@126.com>
2016-09-25 11:24:30 +0800

    2016/9/25

--- app/src/main/java/com/example/yih/smartbj/SplashActivity.java
+++ app/src/main/java/com/example/yih/smartbj/SplashActivity.java
@@ -1,7 +1,10 @@
 package com.example.yih.smartbj;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.yih.smartbj.utils.SPTools;
 
 public class SplashActivity extends Activity {
 
@@ -9,5 +12,14 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        initView();
+    }
+
+    public void initView(){
+        if (SPTools.getBool(this,Mycontast.ISFISRTRUN,false)){
+            startActivity(new Intent(this,GuidActivity.class));
+        }else{
+            startActivity(new Intent(this,HomeActivity.class));
+        }
     }
 }
